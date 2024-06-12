da81c402ac49e687e0ef5d8c72f81de3a1bb5826
felliperfm <felliperfm@gmail.com>
2024-06-12 14:52:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const barbearia = {
         { id: 4, tipo: "Moicano", valor: 10 },
         { id: 5, tipo: "Razor part", valor: 5 },
     ],
-  
+
     barbas: [
         { id: 1, tipo: "Capitão Jack", valor: 20 },
         { id: 2, tipo: "Van Dyke", valor: 20 },
@@ -16,30 +16,54 @@ const barbearia = {
     ],
     estaAberto: true,
 };
-  
-  
+
+
 function buscaCortePorId(id) {
+    for (i = 0; i < barbearia.cortes.length; i++) {
+        if (barbearia.cortes[i].id == id) {
+            return barbearia.cortes[i]
+        }
+    } return "Corte não encontrado"
 }
 
 function buscaBarbaPorId(id) {
+    for (i = 0; i < barbearia.barbas.length; i++) {
+        if (barbearia.barbas[i].id == id) {
+            return barbearia.barbas[i]
+        }
+    } return "Barba não encontrada"
 }
 
 function verificaStatusBarbearia() {
+    return barbearia.estaAberto ? "Estamos abertos" : "Estamos fechados"
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    let pedido = {}
+    let corte = buscaCortePorId(corteId)
+    let barba = buscaBarbaPorId(barbaId)
+    pedido.nome = nomeCliente
+    pedido.pedidoCorte = corte.tipo
+    pedido.pedidoCortePreco = corte.valor
+    pedido.pedidoBarba = barba.tipo
+    pedido.pedidoBarbaPreco = barba.valor
+    return pedido
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    barbearia[lista][id - 1].valor = valor;
+    barbearia[lista][id - 1].tipo = tipo;
+    return barbearia[lista]
 }
 
 function calculaTotal(pedido) {
+    return pedido.pedidoCortePreco + pedido.pedidoBarbaPreco
 }
-  
-  
